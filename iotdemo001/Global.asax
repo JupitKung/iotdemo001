7e3f845250d8b915ceb82ef0f679811d1b60fc6c
Praiya Jupiter <praiya_kung@hotmail.com>
2017-09-09 16:13:32 +0700

    Add project files.

---
+++ iotdemo001/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iotdemo001.Global" Language="C#" %>
---
+++ iotdemo001/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iotdemo001.Startup))]
+namespace iotdemo001
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
